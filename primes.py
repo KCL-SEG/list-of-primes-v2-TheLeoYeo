66284a1d9f219bae3af64b1a42518cc976b7debd
TheLeoYeo <leo.yeofr@gmail.com>
2022-10-21 23:39:03 +0100

    first try

--- primes.py
+++ primes.py
@@ -3,4 +3,23 @@
 
 def primes(number_of_primes):
     list = []
+    if number_of_primes <= 0:
+        raise ValueError(f"{number_of_primes} is too small")
+    
+    list.append(2)
+    if number_of_primes == 1:
+        return list
+
+    current_num = 3
+    while len(list) < number_of_primes:
+        divisible = False
+        length = len(list)
+        for num in list[1:length//2]:
+            if current_num % num == 0:
+                divisible = True
+                break
+        if not divisible:
+            list.append(current_num)
+        current_num += 2
     return list
+
